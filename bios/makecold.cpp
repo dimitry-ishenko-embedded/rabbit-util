78ebb235c34071598206df8c088a97563d3c9e20
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2023-10-28 16:57:40 -0400

    bios: tweak copyright messages

--- bios/makecold.cpp
+++ bios/makecold.cpp
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2023 Dimitry Ishenko <dimitry.ishenko (at) (gee) mail (dot) com>
  * Copyright (c) 2020 Digi International Inc.
+ * Copyright (c) 2023 Dimitry Ishenko <dimitry (dot) ishenko (at) (gee) mail (dot) com>
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
